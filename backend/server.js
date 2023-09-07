c1d991726880a488498fa191acb44fb5b1f82a83
vigkrishna <kvig1_be22@thapar.edu>
2023-09-07 15:08:50 +0530

    routes inititated

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,26 @@ app.get("/", (req,res)=>{
     res.send("main landing page");
 })
 
+app.get("/login", (req,res)=>{
+    res.send("loginpage");
+})
+
+app.get("/signup", (req,res)=>{
+    res.send("signup");
+})
+
+app.get("/quizpage", (req,res)=>{
+    res.send("quizpage");
+})
+
+app.get("/results", (req,res)=>{
+    res.send("results");
+})
+
+
+
+
+
 app.listen(PORT, (req,res)=>{
     console.log("server is running");
 })
